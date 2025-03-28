483065923d74b57b25c3b2cb4f281633f89daece
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-28 15:38:11 +0500

    user router added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import Elysia from 'elysia';
+import swagger from '@elysiajs/swagger';
+import { UserRouter } from './user/user.router';
 
 const PORT = process.env.PORT ?? 4000;
 
 const app = new Elysia();
+
+app.use(swagger());
+app.use(UserRouter);
 
 app.listen(PORT);
