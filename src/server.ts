cec9f5c73aeacae0502c57007c9752e398579c02
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-28 18:52:45 +0500

    auth added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Elysia from 'elysia';
 import swagger from '@elysiajs/swagger';
+import { AuthRouter } from './auth/auth.router';
 import { UserRouter } from './user/user.router';
 
 const PORT = process.env.PORT ?? 4000;
@@ -7,6 +8,7 @@ const PORT = process.env.PORT ?? 4000;
 const app = new Elysia();
 
 app.use(swagger());
+app.use(AuthRouter);
 app.use(UserRouter);
 
 app.listen(PORT);
