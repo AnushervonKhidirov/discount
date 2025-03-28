540424f9bf1d388ca1a95df1fdc47f2d5d7359c0
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-28 15:37:52 +0500

    feat: user service added

---
+++ src/user/user.service.ts
@@ -0,0 +1,72 @@
+import type { Prisma, User } from '@prisma/client';
+import type { ReturnPromiseWithErr } from '@type/return-with-error.type';
+import type { CreateUserDto } from './dto/create-user.dto';
+import type { UpdateUserDto } from './dto/update-user.dto';
+
+import { PrismaClient } from '@prisma/client';
+import { exceptionHelper } from '@helper/exception.helper';
+import { NotFoundException, ConflictException } from '@exception';
+
+export class UserService {
+  private readonly repository = new PrismaClient().user;
+
+  async findOne(where: Prisma.UserWhereUniqueInput): ReturnPromiseWithErr<Omit<User, 'password'>> {
+    try {
+      const user = await this.repository.findUnique({ where, omit: { password: true } });
+      if (!user) throw new NotFoundException('User not found');
+      return [user, null];
+    } catch (err) {
+      return exceptionHelper(err, true);
+    }
+  }
+
+  async findMany(where?: Prisma.UserWhereInput): ReturnPromiseWithErr<Omit<User, 'password'>[]> {
+    try {
+      const users = await this.repository.findMany({ where, omit: { password: true } });
+      return [users, null];
+    } catch (err) {
+      return exceptionHelper(err, true);
+    }
+  }
+
+  async create(createUserDto: CreateUserDto): ReturnPromiseWithErr<Omit<User, 'password'>> {
+    try {
+      const [isExist] = await this.findOne({ username: createUserDto.username });
+      if (isExist) throw new ConflictException(`User '${createUserDto.username}' already exists`);
+
+      createUserDto.password = await Bun.password.hash(createUserDto.password, 'bcrypt');
+
+      const user = await this.repository.create({ data: createUserDto, omit: { password: true } });
+      return [user, null];
+    } catch (err) {
+      return exceptionHelper(err, true);
+    }
+  }
+
+  async update(
+    id: number,
+    updateUserDto: UpdateUserDto,
+  ): ReturnPromiseWithErr<Omit<User, 'password'>> {
+    try {
+      const [_, err] = await this.findOne({ id });
+      if (err) throw err;
+
+      const user = await this.repository.update({ data: updateUserDto, where: { id } });
+      return [user, null];
+    } catch (err) {
+      return exceptionHelper(err, true);
+    }
+  }
+
+  async delete(id: number): ReturnPromiseWithErr<Omit<User, 'password'>> {
+    try {
+      const [_, err] = await this.findOne({ id });
+      if (err) throw err;
+
+      const user = await this.repository.delete({ where: { id } });
+      return [user, null];
+    } catch (err) {
+      return exceptionHelper(err, true);
+    }
+  }
+}
