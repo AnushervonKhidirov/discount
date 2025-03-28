6854ba0499981b900a531440b7478723ca3d1fa4
Anushervon Khidirov <ankhid98@gmail.com>
2025-03-28 21:32:24 +0500

    feat: auth middleware added

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -2,6 +2,8 @@ import Elysia, { t } from 'elysia';
 
 import { UserService } from './user.service';
 import { updateUserBody } from './dto/update-user.dto';
+import { authMiddleware } from '../common/middleware/auth.middleware';
+import { ForbiddenException } from '@exception';
 
 export const UserRouter = new Elysia({ prefix: 'users' });
 const userService = new UserService();
@@ -22,22 +24,23 @@ UserRouter.get(
   { params: t.Object({ id: t.Number() }) },
 );
 
-UserRouter.patch(
-  '/:id',
-  async ({ params, body, error }) => {
-    const [user, err] = await userService.update(params.id, body);
+UserRouter.use(authMiddleware).patch(
+  '/',
+  async ({ store, body, error }) => {
+    const [user, err] = await userService.update(store.userId, body);
     if (err) throw error(err.status, { ...err });
     return user;
   },
-  { params: t.Object({ id: t.Number() }), body: updateUserBody },
+  { body: updateUserBody },
 );
 
-UserRouter.delete(
-  '/:id',
-  async ({ params, error }) => {
-    const [user, err] = await userService.delete(params.id);
-    if (err) throw error(err.status, { ...err });
-    return user;
-  },
-  { params: t.Object({ id: t.Number() }) },
-);
+UserRouter.use(authMiddleware).delete('/', async ({ store, error }) => {
+  if (store.role === 'SUPER_ADMIN') {
+    const exception = new ForbiddenException();
+    throw error(exception.status, { ...exception });
+  }
+
+  const [user, err] = await userService.delete(store.userId);
+  if (err) throw error(err.status, { ...err });
+  return user;
+});
