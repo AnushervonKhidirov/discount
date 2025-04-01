47ccb8e62968e7e448bb53bbabe4e8eca553f064
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-01 13:16:13 +0500

    feat: upload service updated

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,39 +1,54 @@
-import { NotFoundException } from '@exception';
 import type { UploadDto } from './dto/upload.dto';
+import type { ReturnPromiseWithErr } from '@type/return-with-error.type';
 
-import { file } from 'elysia';
+import { ElysiaFile, file } from 'elysia';
 import { exists, writeFile, mkdir, unlink } from 'fs/promises';
 import { join } from 'path';
 import { v4 as uuid } from 'uuid';
+import { InternalServerErrorException, NotFoundException } from '@exception';
+import { exceptionHelper } from '@helper/exception.helper';
 
 export class UploadService {
-  async get(path: string, fileName: string) {
-    const fullPath = join(process.cwd(), 'uploads', path, fileName);
-    const isExist = await exists(fullPath);
-    if (!isExist) return new NotFoundException('File not found');
-    return file(fullPath);
+  async get(path: string, fileName: string): ReturnPromiseWithErr<ElysiaFile> {
+    try {
+      const fullPath = join(process.cwd(), 'uploads', path, fileName);
+      const isExist = await exists(fullPath);
+      if (!isExist) throw new NotFoundException('File not found');
+      return [file(fullPath), null];
+    } catch (err) {
+      return exceptionHelper(err);
+    }
   }
 
-  async create({ file, path, fileName }: UploadDto) {
-    const format = file.name.split('.').at(-1);
-    if (!format) return;
+  async create({ file, path, fileName }: UploadDto): ReturnPromiseWithErr<string> {
+    try {
+      const format = file.name.split('.').at(-1);
+      if (!format) throw new InternalServerErrorException('Unable to save file');
 
-    const fullPath = join(process.cwd(), 'uploads', path);
+      const fullPath = join(process.cwd(), 'uploads', path);
 
-    await this.createFolderIfNotExist(fullPath);
+      await this.createFolderIfNotExist(fullPath);
 
-    const fileBuffer = Buffer.from(await file.arrayBuffer());
-    const name = `${fileName}-${uuid()}.${format}`;
+      const fileBuffer = Buffer.from(await file.arrayBuffer());
+      const name = `${fileName}-${uuid()}.${format}`;
 
-    await writeFile(join(fullPath, name), fileBuffer);
-    return join(path, name);
+      await writeFile(join(fullPath, name), fileBuffer);
+      return [join(path, name), null];
+    } catch (err) {
+      return exceptionHelper(err);
+    }
   }
 
-  async delete(path: string, fileName: string) {
-    const fullPath = join(process.cwd(), 'uploads', path, fileName);
-    const isExist = await exists(fullPath);
-    if (!isExist) return new NotFoundException('File not found');
-    await unlink(fullPath);
+  async delete(path: string, fileName: string): ReturnPromiseWithErr<unknown> {
+    try {
+      const fullPath = join(process.cwd(), 'uploads', path, fileName);
+      const isExist = await exists(fullPath);
+      if (!isExist) throw new NotFoundException('File not found');
+      await unlink(fullPath);
+      return [{}, null];
+    } catch (err) {
+      return exceptionHelper(err);
+    }
   }
 
   private async createFolderIfNotExist(path: string) {
