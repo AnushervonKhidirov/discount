9082dfaae8fee5a7a5d9563c5a1a0af551ebcba2
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-01 13:16:46 +0500

    feat: upload company image api and service added

--- src/company/company.router.ts
+++ src/company/company.router.ts
@@ -3,6 +3,7 @@ import { createCompanyBody } from './dto/create-company.dto';
 import { updateCompanyBody } from './dto/update-company.dto';
 import { authMiddleware } from '@middleware/auth.middleware';
 import { CompanyService } from './company.service';
+import { uploadBody } from '../upload/dto/upload.dto';
 
 export const CompanyRouter = new Elysia({ prefix: 'companies' });
 const companyService = new CompanyService();
@@ -36,7 +37,7 @@ CompanyRouter.use(authMiddleware).post(
 );
 
 CompanyRouter.use(authMiddleware).patch(
-  '/',
+  '/:id',
   async ({ body, params, store, error }) => {
     const [company, err] = await companyService.update(params.id, body, store.userId);
     if (err) throw error(err.status, { ...err });
@@ -49,13 +50,26 @@ CompanyRouter.use(authMiddleware).patch(
 );
 
 CompanyRouter.use(authMiddleware).delete(
-  '/',
+  '/:id',
   async ({ params, store, error }) => {
     const [company, err] = await companyService.delete(params.id, store.userId);
     if (err) throw error(err.status, { ...err });
     return company;
   },
   {
+    params: t.Object({ id: t.Number() }),
+  },
+);
+
+CompanyRouter.use(authMiddleware).post(
+  '/upload-logo/:id',
+  async ({ params, body, store, error }) => {
+    const [company, err] = await companyService.uploadLogo(params.id, body.file, store.userId);
+    if (err) throw error(err.status, { ...err });
+    return company;
+  },
+  {
+    body: uploadBody,
     params: t.Object({ id: t.Number() }),
   },
 );
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -6,9 +6,11 @@ import type { UpdateCompanyDto } from './dto/update-company.dto';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { exceptionHelper } from '@helper/exception.helper';
 import { NotFoundException } from '@exception';
+import { UploadService } from '../upload/upload.service';
 
 export class CompanyService {
   private readonly repository = new PrismaClient().company;
+  private readonly uploadService = new UploadService();
 
   async findOne(where: Prisma.CompanyWhereUniqueInput): ReturnPromiseWithErr<Company> {
     try {
@@ -44,7 +46,7 @@ export class CompanyService {
     userId: number,
   ): ReturnPromiseWithErr<Company> {
     try {
-      const [_, err] = await this.findOne({ id });
+      const [_, err] = await this.findOne({ id, userId });
       if (err) throw err;
 
       const companies = await this.repository.update({
@@ -60,11 +62,37 @@ export class CompanyService {
 
   async delete(id: number, userId: number): ReturnPromiseWithErr<Company> {
     try {
-      const [_, err] = await this.findOne({ id });
+      const [_, err] = await this.findOne({ id, userId });
       if (err) throw err;
 
       const companies = await this.repository.delete({ where: { id, userId } });
       return [companies, null];
+    } catch (err) {
+      return exceptionHelper(err, true);
+    }
+  }
+
+  async uploadLogo(id: number, file: File, userId: number): ReturnPromiseWithErr<Company> {
+    try {
+      const [company, err] = await this.findOne({ id, userId });
+      if (err) throw err;
+
+      const companyImageName = `${company.name.replaceAll(' ', '_')}_id_${company.id}`;
+
+      const [logoUrl, logoErr] = await this.uploadService.create({
+        file,
+        path: 'logos',
+        fileName: companyImageName,
+      });
+
+      if (logoErr) throw logoErr;
+
+      const updatedCompany = await this.repository.update({
+        data: { logoUrl },
+        where: { id, userId },
+      });
+
+      return [updatedCompany, null];
     } catch (err) {
       return exceptionHelper(err, true);
     }
