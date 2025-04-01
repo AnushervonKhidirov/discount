cb99fdac6b3b4623b52ce6a4c6b3591812f19ff0
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-01 12:17:21 +0500

    feat: uploads api and service added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Elysia from 'elysia';
 import swagger from '@elysiajs/swagger';
 import { AuthRouter } from './auth/auth.router';
 import { UserRouter } from './user/user.router';
+import { UploadRouter } from './upload/upload.router';
 
 const PORT = process.env.PORT ?? 4000;
 
@@ -10,5 +11,6 @@ const app = new Elysia();
 app.use(swagger());
 app.use(AuthRouter);
 app.use(UserRouter);
+app.use(UploadRouter)
 
 app.listen(PORT);
