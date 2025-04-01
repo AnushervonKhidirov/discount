443c6e678844ba9844232c88d10e740231a799a7
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-01 20:25:58 +0500

    feat: prisma exception added

--- src/common/helper/exception.helper.ts
+++ src/common/helper/exception.helper.ts
@@ -1,9 +1,23 @@
 import { logger } from '@config/logger';
-import { HttpException, InternalServerErrorException } from '@exception';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { ConflictException, HttpException, InternalServerErrorException } from '@exception';
+import { PrismaCode } from '../constant/prisma-code';
 
 export function exceptionHelper(err: unknown, logError?: boolean): [null, HttpException] {
   const isHttpException = err instanceof HttpException;
   if (logError && !isHttpException) logger.error(err);
+
+  if (err instanceof PrismaClientKnownRequestError) {
+    const exception = prismaException(err);
+    if (exception) return [null, exception];
+  }
+
   const exception = isHttpException ? err : new InternalServerErrorException();
   return [null, exception];
+}
+
+function prismaException(err: PrismaClientKnownRequestError) {
+  if (err.code === PrismaCode.Unique_Constraint_Failed) {
+    return new ConflictException();
+  }
 }
