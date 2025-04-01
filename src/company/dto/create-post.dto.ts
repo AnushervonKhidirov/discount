fd3a3a0fcad7d729f54462c57446375b551f863f
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-01 11:07:21 +0500

    feat: remove logoUrl field from company dto

--- src/company/dto/create-post.dto.ts
+++ src/company/dto/create-post.dto.ts
@@ -3,7 +3,6 @@ import { t } from 'elysia';
 export const CreatePostBody = t.Object({
   name: t.String(),
   about: t.Optional(t.String()),
-  logoUrl: t.Optional(t.String()),
 });
 
 export type CreatePostDto = typeof CreatePostBody.static;
--- src/company/dto/update-post.dto.ts
+++ src/company/dto/update-post.dto.ts
@@ -3,7 +3,6 @@ import { t } from 'elysia';
 export const UpdatePostBody = t.Object({
   name: t.Optional(t.String()),
   about: t.Optional(t.String()),
-  logoUrl: t.Optional(t.String()),
 });
 
 export type UpdatePostDto = typeof UpdatePostBody.static;
