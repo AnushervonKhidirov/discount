af4de9b176a4796ab1c0d461fdddb903872a6cd4
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-01 10:02:06 +0500

    feat company router added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Elysia from 'elysia';
 import swagger from '@elysiajs/swagger';
 import { AuthRouter } from './auth/auth.router';
 import { UserRouter } from './user/user.router';
+import { CompanyRouter } from './company/company.router';
 
 const PORT = process.env.PORT ?? 4000;
 
@@ -10,5 +11,6 @@ const app = new Elysia();
 app.use(swagger());
 app.use(AuthRouter);
 app.use(UserRouter);
+app.use(CompanyRouter);
 
 app.listen(PORT);
