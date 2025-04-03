a8c9975aef811b4c02ec24586f1e1a3d0df6384c
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-03 13:51:56 +0500

    fix: role middleware fixed

--- src/common/middleware/role-middleware.ts
+++ src/common/middleware/role-middleware.ts
@@ -1,15 +1,26 @@
 import type { $Enums } from '@prisma/client';
-
-import Elysia from 'elysia';
-import { authMiddleware } from './auth.middleware';
-import { ForbiddenException } from '@exception';
+import { error } from 'elysia';
+import { ForbiddenException, UnauthorizedException } from '@exception';
 
 export const roleMiddleware = (roles: $Enums.Role[]) => {
-  return (app: Elysia) =>
-    app.use(authMiddleware).onBeforeHandle(({ store, error }) => {
+  return {
+    beforeHandle: ({
+      store,
+    }: {
+      store?: {
+        userId: number;
+        role: $Enums.Role;
+      };
+    }) => {
+      if (!store?.role) {
+        const err = new UnauthorizedException();
+        throw error(err.status, { ...err });
+      }
+
       if (!roles.includes(store.role)) {
         const err = new ForbiddenException();
         throw error(err.status, { ...err });
       }
-    });
+    },
+  };
 };
