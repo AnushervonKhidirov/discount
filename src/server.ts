254c41f62037b267b2c599f47deea50f875e27ce
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-06 11:52:03 +0500

    db data moved

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import Elysia from 'elysia';
 import swagger from '@elysiajs/swagger';
+import { cors } from '@elysiajs/cors';
+
 import { AuthRouter } from './auth/auth.router';
 import { UserRouter } from './user/user.router';
 import { CompanyRouter } from './company/company.router';
@@ -14,6 +16,8 @@ const PORT = process.env.PORT ?? 4000;
 const app = new Elysia();
 
 app.use(swagger());
+app.use(cors());
+
 app.use(AuthRouter);
 app.use(UserRouter);
 app.use(CompanyRouter);
