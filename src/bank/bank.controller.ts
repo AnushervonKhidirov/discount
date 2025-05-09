22f3c5dbd1d8411621e5694434450baa0404242c
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-09 22:59:13 +0500

    feat: bank swagger response example added

--- src/bank/bank.controller.ts
+++ src/bank/bank.controller.ts
@@ -1,10 +1,21 @@
+import { ApiResponse } from '@nestjs/swagger';
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { BankService } from './bank.service';
+
+const bank = {
+  id: 1,
+  name: 'Alif',
+  logoUrl: null,
+  archived: false,
+  createdAt: '2025-05-08T16:31:28.000Z',
+  updatedAt: '2025-05-08T16:31:28.000Z',
+};
 
 @Controller('banks')
 export class BankController {
   constructor(private readonly bankService: BankService) {}
 
+  @ApiResponse({ example: [bank] })
   @Get()
   async findMany() {
     const [banks, err] = await this.bankService.findMany();
@@ -12,6 +23,7 @@ export class BankController {
     return banks;
   }
 
+  @ApiResponse({ example: bank })
   @Get('/:id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const [bank, err] = await this.bankService.findOne({ id });
