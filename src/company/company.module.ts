59966a3475a109c094fb66f91dbbbac5a64b0a28
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-13 11:28:16 +0500

    feat: TokenModule imported

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { TokenModule } from 'src/token/token.module';
 import { UploadModule } from 'src/upload/upload.module';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 
 @Module({
-  imports: [PrismaModule, UploadModule],
+  imports: [PrismaModule, TokenModule, UploadModule],
   providers: [CompanyService],
   controllers: [CompanyController],
 })
