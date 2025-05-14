ddc9067b210ee0e9e9e78409d9a0e2fca06c7e32
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-14 13:23:19 +0500

    feat: token module added

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { TokenModule } from 'src/token/token.module';
 import { CompanyModule } from 'src/company/company.module';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
 
 @Module({
-  imports: [PrismaModule, CompanyModule],
+  imports: [PrismaModule, CompanyModule, TokenModule],
   controllers: [StoreController],
   providers: [StoreService],
 })
