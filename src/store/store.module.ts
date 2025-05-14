68347483d393f138362f738aaceb5d72252fd81c
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-14 12:31:38 +0500

    feat: add company module to inports

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CompanyModule } from 'src/company/company.module';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CompanyModule],
   controllers: [StoreController],
   providers: [StoreService],
 })
