cc4f3e906aa6cceabf900e18a2307fa03b9070f8
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-14 11:52:06 +0500

    feat: store dto added

---
+++ src/store/dto/create-store.dto.ts
@@ -0,0 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNumber,
+  IsLatitude,
+  IsLongitude,
+  IsMilitaryTime,
+} from 'class-validator';
+
+export class CreateStoreDto {
+  @ApiProperty({ example: 1 })
+  @IsNumber()
+  countryId: number;
+
+  @ApiProperty({ example: 1 })
+  @IsNumber()
+  cityId: number;
+
+  @ApiProperty({ example: 'Ayni street 48' })
+  @IsString()
+  address: string;
+
+  @ApiProperty({ example: 38.57882399486239 })
+  @IsLatitude()
+  latitude: number;
+
+  @ApiProperty({ example: 68.78637547795134 })
+  @IsLongitude()
+  longitude: number;
+
+  @ApiProperty({ example: '8:00' })
+  @IsMilitaryTime()
+  openAt: string;
+
+  @ApiProperty({ example: '21:00' })
+  @IsMilitaryTime()
+  closeAt: string;
+
+  @ApiProperty({ example: 1 })
+  @IsNumber()
+  companyId: number;
+}
---
+++ src/store/dto/update-store.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsBoolean, ValidateIf } from 'class-validator';
+import { CreateStoreDto } from './create-store.dto';
+
+export class UpdateStoreDto extends PartialType(CreateStoreDto) {
+  @ApiProperty({ example: true })
+  @IsBoolean()
+  @ValidateIf(({ archived }) => archived !== undefined)
+  archived: boolean;
+}
