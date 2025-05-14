46a84d001d1fd6d6b4b3e7b6aa3a6789594bad68
Anushervon Khidirov <ankhid98@gmail.com>
2025-05-14 10:59:51 +0500

    feat: add country to dto

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,11 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNumber, IsNotEmpty, ValidateIf } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  IsNotEmpty,
+  ValidateIf,
+  IsArray,
+} from 'class-validator';
 
 export class CreateCompanyDto {
   @ApiProperty({ example: 'Defacto' })
   @IsString()
   @IsNotEmpty()
   name: string;
+
+  @ApiProperty({ example: [1, 2] })
+  @IsArray({ always: true })
+  @IsNumber({}, { each: true })
+  @IsNotEmpty()
+  countryIds: number[];
 
   @ApiProperty({ example: 'Defacto store with high quality brand clothes!' })
   @IsString()
